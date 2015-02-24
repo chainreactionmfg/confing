ce8b79712d6d43aac740a550645ba4e0b0ab0ef3
Fahrzin Hemmati <fahhem@gmail.com>
2015-02-23 17:33:59 -0800

    Add some more useful tests

--- confing/confing.py
+++ confing/confing.py
@@ -21,6 +21,7 @@ class Confer(object):
   def __init__(self, name):
     self._name = name
     self._schema = colander.Schema()
+    self._loaded = None
 
   def define(self, schema_node):
     assert isinstance(schema_node, colander.SchemaNode)
@@ -29,9 +30,27 @@ class Confer(object):
             '%s is already defined on this Confer' % schema_node.title)
     self._schema[schema_node.title] = schema_node
 
+  @property
+  def definitions(self):
+    return self._schema.children
+
   def load(self, cstruct):
-    return LoadedConfig(
+    if self._loaded is not None:
+      raise ValueError('Cannot load the same config twice.')
+    self._loaded = LoadedConfig(
         self._schema.deserialize(cstruct), self._schema)
+    return self._loaded
+
+  @property
+  def loaded(self):
+    return self._loaded
+
+  def AsCallable(self, attr):
+    def ConfigCallable():
+      if self._loaded is None:
+        raise ValueError('Config is not loaded yet.')
+      return self._loaded[attr]
+    return ConfigCallable
 
 
 class LoadedConfig(object):
@@ -45,11 +64,7 @@ class LoadedConfig(object):
   def __getitem__(self, attr):
     data = self._data.get(attr)
     schema = self._schema[attr]
-    if isinstance(schema.schema_type, colander.Mapping):
+    if isinstance(schema.typ, colander.Mapping):
       # Nested type, so wrap it too.
       return LoadedConfig(self._data[attr], schema)
     return data
-
-  def AsCallable(self, attr):
-    return lambda: self[attr]
-
--- tests/test_confing.py
+++ tests/test_confing.py
@@ -6,15 +6,62 @@ test_confing
 Tests for `confing` module.
 """
 import unittest
-import confing
+
+import colander
+
+from confing import confing
 from confing import data
 
 
 class TestConfing(unittest.TestCase):
   @classmethod
-  def set_up(self):
+  def setUp(self):
     confing._confers.clear()
 
   def test_definition(self):
     confing.getConfer('tests').define(data.Int(name='test'))
     assert confing.getConfer('tests').load({'test': 3}).test == 3
+
+  def test_repetitions(self):
+    confer = confing.getConfer('tests')
+
+    # Repeat definition.
+    confer.define(data.Int(name='test'))
+    with self.assertRaises(NameError):
+      confer.define(data.Int(name='test'))
+
+    # Repeat load.
+    confer.load({'test': 1})
+    with self.assertRaises(ValueError):
+      confer.load({'test': 2})
+
+  def test_doccing(self):
+    confer = confing.getConfer('tests')
+    confer.define(data.Int(name='with_doc').Doc('Just a test int'))
+    for defn in confer.definitions:
+      print(defn, defn.__dict__)
+      assert defn.description == 'Just a test int'
+
+  def test_callable(self):
+    confer = confing.getConfer('tests')
+    confer.define(data.Int(name='with_doc').Doc('Just a test int'))
+    callable = confer.AsCallable('with_doc')
+
+    with self.assertRaises(ValueError):
+      callable()
+
+    confer.load({'with_doc': 5})
+    assert callable() == 5
+
+  def test_nested(self):
+    confer = confing.getConfer('tests')
+    confer.define(data.Dict(
+        data.Int(name='first', required=True),
+        data.String(name='second'),
+        name='dict', required=True))
+    with self.assertRaises(colander.Invalid):
+      confer.load({})
+    with self.assertRaises(colander.Invalid):
+      confer.load({'dict': {'second': 'text'}})
+    confer.load({'dict': {'first': 3, 'second': 'text'}})
+    assert confer.loaded.dict.first == 3
